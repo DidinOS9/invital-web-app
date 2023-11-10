c61875dba8f4870d29feb9c2ec74b61095c49dcb
Ahmad Haidar Kamal <61811223+haidar30@users.noreply.github.com>
2023-11-10 23:58:38 +0700

    Update Multi Authentication User
    superadmin@gmail.com pass:12341234
    admin@gmail.com pass:12341234

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,34 +1,43 @@
 <?php
-
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User; 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
-class UserSeeder extends Seeder
+class userSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
+        // Menghapus semua data yang ada di tabel users jika diperlukan
+        User::truncate();
+
+        // Data dummy
         DB::table('users')->insert([
             [
                 'name' => 'Super Admin',
                 'username' => 'superadmin',
                 'email' => 'superadmin@gmail.com',
+                'email_verified_at' => now(),
                 'role' => 'superadmin',
                 'status' => 'active',
-                'password' => bcrypt('12341234')
+                'password' => bcrypt('12341234'),
+                'remember_token' => Str::random(10), // Menggunakan Str::random() untuk menghasilkan token yang acak
+                'created_at' => now(),
+                'updated_at' => now()
             ],
             [
                 'name' => 'Admin',
                 'username' => 'admin',
                 'email' => 'admin@gmail.com',
+                'email_verified_at' => now(),
                 'role' => 'admin',
                 'status' => 'active',
-                'password' => bcrypt('12341234')
+                'password' => bcrypt('12341234'),
+                'remember_token' => Str::random(10), // Menggunakan Str::random() untuk menghasilkan token yang acak
+                'created_at' => now(),
+                'updated_at' => now(),
             ]
         ]);
     }
--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User; 
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class userSeeder extends Seeder
 {
@@ -13,23 +14,31 @@ class userSeeder extends Seeder
         User::truncate();
 
         // Data dummy
-        $users = [
+        DB::table('users')->insert([
             [
-                'name' => 'Administrator',
-                'username' => 'admin',
-                'email' => 'admin@mail.com',
+                'name' => 'Super Admin',
+                'username' => 'superadmin',
+                'email' => 'superadmin@gmail.com',
                 'email_verified_at' => now(),
-                'password' => 'admin',
-                'phone_number' => '123-456-7890',
-                'instagram' => 'kucing_instagram',
+                'role' => 'superadmin',
+                'status' => 'active',
+                'password' => bcrypt('12341234'),
+                'remember_token' => Str::random(10), // Menggunakan Str::random() untuk menghasilkan token yang acak
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'email_verified_at' => now(),
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('12341234'),
                 'remember_token' => Str::random(10), // Menggunakan Str::random() untuk menghasilkan token yang acak
                 'created_at' => now(),
                 'updated_at' => now(),
-            ],
-            // Tambahkan data dummy lainnya di sini
-        ];
-
-        // Menyisipkan data dummy ke dalam tabel 'users' menggunakan model Eloquent
-        User::insert($users);
+            ]
+        ]);
     }
 }
