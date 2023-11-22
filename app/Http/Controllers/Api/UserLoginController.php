47978d2bcba918dec3672bd11a3249b31dccc9de
Miftah Hozuki <miftahzk.payments@gmail.com>
2023-11-22 16:38:05 +0700

    fix api auth login

--- app/Http/Controllers/Api/UserLoginController.php
+++ app/Http/Controllers/Api/UserLoginController.php
@@ -6,21 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Auth;
 
 class UserLoginController extends Controller
 {
 
     public function login(Request $request)
     {
-        $username = $request->input('username');
-        $password = $request->input('password');
+        $credentials = $request->only('username', 'password');
 
-        $user = User::where('username', $username)->where('password', $password)->first();
-
-        if ($user) {
-            return response()->json(['message' => 'berhasil', 'code' => 200, 'user' => $user]);
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            return response()->json(['message' => 'Berhasil', 'code' => 200, 'user' => $user]);
         }
 
-        return response()->json(['message' => 'Email dan password tidak cocok', 'code' => 400]);
+        return response()->json(['message' => 'Username dan password tidak cocok', 'code' => 400]);
     }
+
 }
