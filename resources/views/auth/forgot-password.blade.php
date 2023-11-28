a317a7b6b8539142ef5af1f4c69c66fcb47ea5ba
Ahmad Haidar Kamal <61811223+haidar30@users.noreply.github.com>
2023-11-28 12:22:21 +0700

    Fix Upload

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,37 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@extends('layouts.niceadmin')
+
+@section('content')
+
+    <div class="card mb-3">
+
+        <div class="card-body">
+
+        <div class="pt-4 pb-2">
+            <p class="text-center small">Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+        </div>
+
+        <form method="POST" action="{{ route('password.email') }}" class="row g-3 needs-validation" novalidate>
+            @csrf
+
+            <div class="col-12">
+            <label for="email" class="form-label">Your Email</label>
+            <input type="email" name="email" class="form-control" id="email" required>
+            <div class="invalid-feedback">Please enter a valid Email adddress!</div>
+            </div>
+
+            <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms" required>
+                <label class="form-check-label" for="acceptTerms">I agree and accept the <a href="#">terms and conditions</a></label>
+                <div class="invalid-feedback">You must agree before submitting.</div>
+            </div>
+            </div>
+            <div class="col-12">
+            <button class="btn btn-primary w-100" type="submit">Email Password Reset Link</button>
+            </div>
+        </form>
+
+        </div>
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
